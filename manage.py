e36649d1836fad4e34711a49ebac8514747cae03
kx1135790023 <1135790023@qq.com>
2020-12-22 15:40:24 +0800

    增加manage1.py

---
+++ manage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/22 15:34
+# @Author  : Ke xing
+# @Blog    ：https://blog.csdn.net/weixin_45131345?spm=1000.2123.3001.5113
+from flask import Flask
+app = Flask(__name__)
+app.secret_key = "aaasfdfasdg"
+
+app.debug = True
+@app.route("/")
+def index():
+    b=2
+    a=1
+    return "hello"
+if __name__ == '__main__':
+    app.run()
